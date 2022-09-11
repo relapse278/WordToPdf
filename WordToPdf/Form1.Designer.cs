330b08800424aaa6a3c7002c3d9e13b0caef7290
relapse278 <r.schewtschenko@gmx.net>
2022-09-11 11:38:57 +0700

    Добавьте файлы проекта.

---
+++ WordToPdf/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WordToPdf
+{
+    partial class frmApp
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmApp));
+            this.btnOpenDirectory = new System.Windows.Forms.Button();
+            this.proBarConvertionProcess = new System.Windows.Forms.ProgressBar();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.chbSameFolder = new System.Windows.Forms.CheckBox();
+            this.lblConvertationProgress = new System.Windows.Forms.Label();
+            this.lblDirectoryPath = new System.Windows.Forms.Label();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOpenDirectory
+            // 
+            this.btnOpenDirectory.Location = new System.Drawing.Point(93, 78);
+            this.btnOpenDirectory.Name = "btnOpenDirectory";
+            this.btnOpenDirectory.Size = new System.Drawing.Size(203, 48);
+            this.btnOpenDirectory.TabIndex = 0;
+            this.btnOpenDirectory.Text = "&Выберите папку с WORD-документами для их последующей конвертации в PDF";
+            this.btnOpenDirectory.UseVisualStyleBackColor = true;
+            this.btnOpenDirectory.Click += new System.EventHandler(this.btnOpenDirectory_Click);
+            // 
+            // proBarConvertionProcess
+            // 
+            this.proBarConvertionProcess.Location = new System.Drawing.Point(12, 185);
+            this.proBarConvertionProcess.Name = "proBarConvertionProcess";
+            this.proBarConvertionProcess.Size = new System.Drawing.Size(363, 27);
+            this.proBarConvertionProcess.TabIndex = 2;
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.menuStrip2.Size = new System.Drawing.Size(388, 24);
+            this.menuStrip2.TabIndex = 4;
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem3});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(24, 20);
+            this.toolStripMenuItem1.Text = "?";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem3.Text = "&О программе";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // chbSameFolder
+            // 
+            this.chbSameFolder.AutoSize = true;
+            this.chbSameFolder.Location = new System.Drawing.Point(12, 48);
+            this.chbSameFolder.Name = "chbSameFolder";
+            this.chbSameFolder.Size = new System.Drawing.Size(363, 17);
+            this.chbSameFolder.TabIndex = 5;
+            this.chbSameFolder.Text = "Конвертировать файлы в той же папке, где находится программа";
+            this.chbSameFolder.UseVisualStyleBackColor = true;
+            this.chbSameFolder.CheckedChanged += new System.EventHandler(this.chbSameFolder_CheckedChanged);
+            // 
+            // lblConvertationProgress
+            // 
+            this.lblConvertationProgress.AutoSize = true;
+            this.lblConvertationProgress.Location = new System.Drawing.Point(15, 163);
+            this.lblConvertationProgress.Name = "lblConvertationProgress";
+            this.lblConvertationProgress.Size = new System.Drawing.Size(0, 13);
+            this.lblConvertationProgress.TabIndex = 6;
+            // 
+            // lblDirectoryPath
+            // 
+            this.lblDirectoryPath.AutoSize = true;
+            this.lblDirectoryPath.Location = new System.Drawing.Point(15, 137);
+            this.lblDirectoryPath.Name = "lblDirectoryPath";
+            this.lblDirectoryPath.Size = new System.Drawing.Size(0, 13);
+            this.lblDirectoryPath.TabIndex = 7;
+            // 
+            // frmApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 228);
+            this.Controls.Add(this.lblDirectoryPath);
+            this.Controls.Add(this.lblConvertationProgress);
+            this.Controls.Add(this.chbSameFolder);
+            this.Controls.Add(this.proBarConvertionProcess);
+            this.Controls.Add(this.btnOpenDirectory);
+            this.Controls.Add(this.menuStrip2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmApp";
+            this.Text = "Массовая конвертация WORD в PDF";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenDirectory;
+        private System.Windows.Forms.ProgressBar proBarConvertionProcess;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.CheckBox chbSameFolder;
+        private System.Windows.Forms.Label lblConvertationProgress;
+        private System.Windows.Forms.Label lblDirectoryPath;
+    }
+}
+
---
+++ WordToPdf/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Interop.Word;
+using System.Runtime.InteropServices;
+
+namespace WordToPdf
+{
+    public partial class frmApp : Form
+    {
+        private bool bSameFolder = false;
+        private string strExePath = string.Empty;
+
+        public frmApp()
+        {
+            InitializeComponent();
+            strExePath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+        }
+
+        private void callConvert(string[] files) 
+        {
+            string[] wordFiles = getWordFilesFromFolder(files);
+            if (wordFiles.Length > 0) {
+                proBarConvertionProcess.Maximum = wordFiles.Length;
+                convert(wordFiles);
+            } 
+            else {
+                MessageBox.Show("Подходящих файлов (DOC или DOCX) не обнаружено.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnOpenDirectory_Click(object sender, EventArgs e) 
+        {
+            lblDirectoryPath.Text = string.Empty;
+            lblConvertationProgress.Text = string.Empty;
+            proBarConvertionProcess.Value = 0;
+            string[] files = null;
+
+            if (bSameFolder) {
+                files = Directory.GetFiles(strExePath);
+                callConvert(files);
+                return;
+            }
+
+            DialogResult result;
+
+            using (var fbd = new FolderBrowserDialog()) {
+                result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath)) {
+                    files = Directory.GetFiles(fbd.SelectedPath);
+                    lblDirectoryPath.Text = fbd.SelectedPath;
+                    callConvert(files);
+                }
+            }
+        }
+
+        // Метод выдаёт массив названий всех Word-файлов.
+        private string[] getWordFilesFromFolder(string[] files)
+        {
+            List<string> wordFiles = new List<string>(); // List используется из-за удобства применения метода Add().
+            
+            foreach (string file in files) {
+                if (Path.GetFileName(file)[0] != '~' && (Path.GetExtension(file) == ".docx" || (Path.GetExtension(file) == ".doc")))
+                    wordFiles.Add(file);
+            }
+
+            return wordFiles.ToArray();
+        }
+
+        private void convert(string[] files) 
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            bool isWordClosed = false;
+            Microsoft.Office.Interop.Word.Document wordDocument = null;
+            Microsoft.Office.Interop.Word.Application appWord = new Microsoft.Office.Interop.Word.Application();
+            int counter = 0;
+
+            if (files != null) {                    
+                foreach(string file in files) {
+                    if (Path.GetFileName(file)[0] != '~' && (Path.GetExtension(file) == ".docx" || (Path.GetExtension(file) == ".doc"))) {
+                        wordDocument = appWord.Documents.Open(file);
+                        string pdf = Path.ChangeExtension(file, ".pdf");
+                        if (File.Exists(pdf)) {
+                            MessageBox.Show("Файл \n\n" + pdf + "\n\nуже существует!\n\n\n\nПроцесс конвертации будет прекращён.", "Внимание!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            CloseAll(appWord, wordDocument);
+                            isWordClosed = true;
+                            break;
+                        }
+                        wordDocument.ExportAsFixedFormat(pdf, WdExportFormat.wdExportFormatPDF); // only if the file doesn't exist yet!
+                        wordDocument.Close();
+                        counter++;
+                        lblConvertationProgress.Text = "Конвертировано " + counter + " из " + files.Length;
+                        proBarConvertionProcess.Value += 1;
+                    }
+                }
+                if (counter > 0)
+                    MessageBox.Show("Процесс конвертации успешно закончен.", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            Cursor.Current = Cursors.Default;               
+
+            if (!isWordClosed) //Объект COM, который был отделен от своего базового RCW, использоваться не может. 
+                CloseAll(appWord, wordDocument);            
+        }
+
+        private void CloseAll(Microsoft.Office.Interop.Word.Application appWord, Microsoft.Office.Interop.Word.Document wordDocument) 
+        {               
+            appWord.Quit();
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(appWord);
+            if (wordDocument != null)
+                Marshal.ReleaseComObject(wordDocument);
+            Marshal.ReleaseComObject(appWord);
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            AboutBox1 aboutWindow = new AboutBox1();
+            aboutWindow.Show();
+        }
+
+        private void chbSameFolder_CheckedChanged(object sender, EventArgs e)
+        {
+            bSameFolder = chbSameFolder.Checked;
+            if (bSameFolder == true) {
+                btnOpenDirectory.Text = "&Конвертировать WORD-документы в PDF";
+                lblDirectoryPath.Text = strExePath;
+            } 
+            else {
+                btnOpenDirectory.Text = "&Выберите папку с WORD-документами для их последующей конвертации в PDF";
+                lblDirectoryPath.Text = string.Empty;
+            }
+        }
+    }
+}
